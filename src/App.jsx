4c3be2939111efa60a3b907bb4f8342ef2c6a320
davidiscool <dammyadavid@gmail.com>
2023-10-01 13:39:31 +0100

    Landing page for the extension project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+// import { Button } from 'flowbite-react';
+import Onboard from './pages/Onboard';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/Dashboard';
+import RecordedVid from './pages/RecordedVid';
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Onboard />}/>
+      <Route path='login' element={<Login />}/>
+      <Route path='dashboard' element={<Dashboard />}/>
+      <Route path='display' element={<RecordedVid />}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
